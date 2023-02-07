23a6145f1da2d18f7090b0b4868b3680c7561d8f
gneiru <noelrohi59@gmail.com>
2023-02-07 10:25:43 +0800

    package config1.1

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-		adapter: adapter()
+		adapter: adapter({
+            split: true
+        })
 	},
 
     preprocess: [preprocess({
