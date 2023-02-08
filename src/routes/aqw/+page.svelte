8aef769a66c05dedf77d9783927c74483fbb8e08
gneiru <noelrohi59@gmail.com>
2023-02-08 19:08:33 +0800

    `on click href fix wrong routes`

--- src/routes/aqw/+page.svelte
+++ src/routes/aqw/+page.svelte
@@ -12,8 +12,8 @@
 				AdventureQuest Worlds is a browser-based massively multiplayer online role-playing game
 				released by Artix Entertainment in 2008
 			</p>
-			<button class="btn btn-primary" on:click={() => window.location = '/aqw/raid/calc'}>Raid Maps</button>
-			<button class="btn btn-secondary" on:click={() => window.location = '/aqw/raid/maps'}>Raid Exp Calculator</button>
+			<button class="btn btn-primary" on:click={() => window.location = '/aqw/raid/maps'}>Raid Maps</button>
+			<button class="btn btn-secondary" on:click={() => window.location = '/aqw/raid/calc'}>Raid Exp Calculator</button>
 		</div>
         
 	</div>
